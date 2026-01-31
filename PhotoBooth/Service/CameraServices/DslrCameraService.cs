44f472f53668a6d6cb2c0f6ed59e83fa3a2d7973
Ilschat <ilschat.s@icloud.com>
2026-01-31 17:23:02 +0100

    bug fixing

--- PhotoBooth/Service/CameraServices/DslrCameraService.cs
+++ PhotoBooth/Service/CameraServices/DslrCameraService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using OpenCvSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace PhotoBooth.Service.CameraServices
 {
     internal class DslrCameraService
     {
+        //todo sai -> resize preview but take foto with full size -> posible memory peak on pi 
+        //var preview = new Mat();
+        //Cv2.Resize(fullResFrame, preview, new OpenCvSharp.Size(1920,1080));
+        //PreviewFrameReady?.Invoke(preview.ImEncode(".jpg"));
+
     }
 }
